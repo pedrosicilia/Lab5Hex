cc081be4488561408a3e7f04b527f2c8c76bbfff
Pedrosicilia <pedro@siblaz.com>
2017-10-12 20:40:36 -0400

    Tryna put all

--- src/main.java
+++ src/main.java
@@ -13,29 +13,93 @@ public class main {
         System.out.println("Enter: " +args[0]+ "");
         System.out.println("Enter: ");
         userinput = scan.nextLine();
+           userinput = userinput.toUpperCase();
 
             char[] charArray = userinput.toCharArray();
 
-            String compare0x = "0x";
+            String compare0x = "0X";
 
+
+           args[0] = args[0].toUpperCase();
             if(args[0].substring(0,2).equals(compare0x))
             {
                 args[0] = args[0].substring(2);
                 System.out.println( "W/O the two chars: " +args[0]+ "");
             }
-            else
-                System.out.println("FAILED");
-            
 
         while (count < userinput.length()) {
             char hex;
             hex = userinput.charAt(count);
             charArray[count] = hex;
 
-            System.out.println("Word/Num: " +charArray[count]+ "");
+            System.out.println("Word/Num: " +count+ "" +charArray[count]+ "");
             count++;
+        }
+
+
+
+            double decimal = 0;
+            double arraylength = charArray.length;
+            double tempdec = 0;
+
+            int charvert = 0;
+
+System.out.println(" LeNGTH: " +arraylength+"");
+
+        for(int i = 0; i < charArray.length; i++) {
+            arraylength--;
+           // decimal = Math.pow(16, arraylength);
+
+                if(charArray[i] == 'A') {
+                    decimal = decimal * 10;
+                }
+                else if(charArray[i] == 'B')
+                    decimal = decimal * 11;
+                else if(charArray[i] == 'C')
+                    decimal = decimal * 12;
+                else if(charArray[i] == 'D' )
+                    decimal = decimal * 13;
+                else if(charArray[i] == 'E')
+                    decimal = decimal * 14;
+                else if(charArray[i] == 'F')
+                    decimal = decimal * 15;
+
+
+              //  charArray[i] = (char) (charArray[i] - 55);
+            charvert = (int)(charArray[i] - '0');
+            System.out.println( "Charverted: " +charvert+ "");
+
+            decimal = Math.pow(16, arraylength) * charvert + decimal;
+
+/*
+            decimal = decimal * charvert;
+
+            tempdec = decimal + tempdec;
+*/
+
+                System.out.println("Char value: "+charArray[i]+ " Position: " +arraylength+ "  DECIMAL: " +decimal+ " TEMPDEC" +tempdec+ "");
+
+
 
         }
+
+        System.out.println( "Converted: " +decimal+ "");
+
+
+        int test;
+        char a = 'A';
+        char f = 'F';
+
+        test = a + f;
+        System.out.println("Expected 135 : " +test+ "");
+
+
+
+
+
+
+
+
 
 
         //conversion();
